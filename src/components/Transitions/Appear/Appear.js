1b440396969021f9830cbbbde0df58023cc806a2
Sorin Fratila <sorin.fratila@protonmail.ch>
2020-04-25 20:23:22 +0200

    added responsiveness to the menu button

--- src/components/Transitions/Appear/Appear.js
+++ src/components/Transitions/Appear/Appear.js
@@ -5,15 +5,15 @@ import { Transition } from 'react-transition-group';
 function appear({ duration, inProp, children }) {
   const defaultStyle = {
     transition: `all ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94)`,
-    transform: 'translate3d(0, -10px, 0)',
+    transform: 'translate3d(0, -20px, 0)',
     opacity: 0,
   };
 
   const transitionStyles = {
     entering: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
     entered: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
-    exiting: { opacity: 0, transform: 'translate3d(0, -10px, 0)' },
-    exited: { opacity: 0, transform: 'translate3d(0, -10px, 0)' },
+    exiting: { opacity: 0, transform: 'translate3d(0, -20px, 0)' },
+    exited: { opacity: 0, transform: 'translate3d(0, -20px, 0)' },
   };
 
   return (
