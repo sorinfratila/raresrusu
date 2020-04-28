d2e2359e564bd6dbc761314dc44ac516969a3cb2
Sorin Fratila <sorin.fratila@protonmail.ch>
2020-04-28 16:07:06 +0200

    added ismobile package to detect when on mobile device browser

--- src/components/ProjectTile/ProjectTile.js
+++ src/components/ProjectTile/ProjectTile.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import classes from './ProjectTile.module.scss';
 import imgArr from '../../assets/images/portfolio';
+import { isMobile } from 'react-device-detect';
 // import Img from 'gatsby-image';
 
 function projectTile({ to }) {
+  const classesArr = [classes.ProjectTile];
+  if (isMobile) {
+    classesArr.push('mobile');
+  }
+
   return (
-    <Link className={classes.ProjectTile} to={to}>
+    <Link className={classesArr.join(' ')} to={to}>
       <span className={classes.opaque_overlay}></span>
       <img className={classes.img_bg} src={imgArr[0]}></img>
       <span className={classes.wording}>
